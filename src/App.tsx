35286747ca05898fb77d7ac75db672f6a8aa7900
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-23 17:19:19 +0300

    Add states to App and props to Quesion Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 //Components
 import { QuestionCard } from './components/QuestionCard';
 
+
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () => {
 
@@ -26,7 +35,14 @@ const App = () => {
         Score:
       </p>
       <p>Loading questions ...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNumber={number+ 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+       />
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
