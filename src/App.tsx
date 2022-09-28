504b91ee94059274b670b2d88ce9cf3d48880834
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-28 14:48:44 +0300

    Fetch data from  API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
+import { fetchQuizQuestions } from './API';
 //Components
 import { QuestionCard } from './components/QuestionCard';
+//Types
+import { Difficulty } from './API';
 
 
 const TOTAL_QUESTIONS = 10;
@@ -12,6 +15,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () => {
 
@@ -35,14 +40,14 @@ const App = () => {
         Score:
       </p>
       <p>Loading questions ...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNumber={number+ 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answer}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-       />
+       /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
