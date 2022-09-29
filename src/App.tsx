153f63e0e4589b752e770d73d8c92d4bb6290f3e
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-29 18:41:17 +0300

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QuestionCard } from './components/QuestionCard';
 //Types
 import { QuestionState, Difficulty } from './API';
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string,
   answer: string,
   correct: boolean,
@@ -74,7 +74,7 @@ const App = () => {
       ) : null}
       {!gameOver ?
         <p className='score'>
-          Score:
+          Score: {score}
         </p> : null
       }
       {loading ?
